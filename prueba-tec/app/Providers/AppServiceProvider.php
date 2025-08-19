3cef757c8db4a290b9616a17ef8332587920bfc1
rjm019 <ricardojimenez@micorreo.upp.edu.mx>
2025-08-19 03:40:46 -0600

    first commit

--- prueba-tec/app/Providers/AppServiceProvider.php
+++ prueba-tec/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Schema;
 
 
@@ -22,5 +23,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+         $url = config('app.url');
+        if ($url) {
+            URL::forceRootUrl($url);
+            if (str_starts_with($url, 'https://')) {
+                URL::forceScheme('https');
+            }
+        }
     }
 }
